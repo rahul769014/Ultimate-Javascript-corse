6b18f6fe1a1cac3e7531ccd5f14f186b32d78917
rahul769014 <111835631+rahul769014@users.noreply.github.com>
2026-05-12 14:28:05 +0000

    continar

---
+++ 03_var_let_const/index.js
@@ -0,0 +1,15 @@
+console.log("JavaScript 3: var,let and const");
+var a =47;
+var b ="Hello";
+const b = "p"
+var c = null
+var d = undefined;
+
+let m = undefined
+console.log(m);
+
+{
+    let b= 'this';
+    console.log(b);
+}
+console.log(b);
