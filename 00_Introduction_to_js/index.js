2fb774dcb7a598f82084dd1dc9403167d543d6d3
rahul769014 <111835631+rahul769014@users.noreply.github.com>
2026-05-12 11:40:56 +0000

    container config done

---
+++ 00_Introduction_to_js/index.js
@@ -0,0 +1,8 @@
+console.log("rahul")
+
+function addthreeNumber(a,b,c) {
+    return a+b+c;
+}
+
+let c = addthreeNumber(1,55,10)
+console.log(c)
---
+++ 02_Variables/index.js
@@ -0,0 +1,9 @@
+var a = 67; //a contains 67
+console.log(a);
+
+ a="harry"
+console.log(a)
+
+// let 56uyg = 8
+
+// let var = 7;
